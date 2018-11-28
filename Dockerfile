4c62dcff20220aefdfc9620eee96145175b0b1a8
Weiwei Yang <wwei@apache.org>
2018-11-28 16:43:02 +0800

    dir change

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM php:7.2-apache
+COPY sample.db /tmp
 COPY index.php /var/www/html/
--- index.php
+++ index.php
@@ -4,9 +4,10 @@
   </head>
   <body>
   <?php
-  $db = new SQLite3('/mnt/data/sample.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+  $db = new SQLite3('/tmp/sample.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
   $results = $db->query('SELECT * FROM "emp"');
 
+  echo "<h2>Following table content is loaded from SQLite3 database, db file: /mnt/data/sample.db</h2>"
   echo "<table border='1'>
     <tr>
       <th>Employ NO</th>
